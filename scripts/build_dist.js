f410c92b90fbe409dd83119add741382a04e2d7c
girobusan <girobusan@gmail>
2021-11-02 21:11:03 +0300

    Preparing to release

--- scripts/build_dist.js
+++ scripts/build_dist.js
@@ -1,7 +1,9 @@
 const fs = require('fs');
 const fse = require('fs-extra');
-const {exec} = require('child_process');
+const {execSync} = require('child_process');
 const ncp = require('ncp').ncp;
+const pkg = JSON.parse(fs.readFileSync('./package.json' , {encoding: "utf8"}))
+console.log("Building version" , pkg.version)
 
 //check dist dir
 if(fs.existsSync('dist')){
@@ -12,7 +14,8 @@ fs.rmdirSync('dist' , {recursive:true});
 //create new
 fs.mkdirSync('dist');
 function runCommand(c){
-  exec(c , (err,stdout,stderr)=>
+  console.log("Command:", c);
+  execSync(c , (err,stdout,stderr)=>
   {
     if(err){
       console.log("Error build" , err);
@@ -29,4 +32,7 @@ runCommand('npm run build_cli');
 
 //drop the dropins
 //list the dropins
+console.log("Filling distribution folder...")
 fse.copySync('dropins/dist/' , 'dist');
+fse.copySync('dist' , 'latid-' + pkg.version);
+console.log("All done.")
